cdfcb7754bac4e300bf0a89ec984c702e121cf6f
Eric <eric@fullinpartners.com>
2018-12-06 12:58:31 -0500

    redirect parent window

--- js/results.js
+++ js/results.js
@@ -263,5 +263,6 @@ console.log("unicornPercent: " + unicornPercent)
 if(redirect != 1) {
     var params = "un=" + unicornPercent + "&ag=" + ageInYears + "&ra=" + totalRaised + "&nr=" + rounds + "&re=" + priorARRRaw + "&rg=" + arrGrowthRaw
     var page = similarCompany(unicornStatus)
-    window.location.replace("https://fullinpartners.com/" + page + "?" + params)
+    window.top.location.href = "https://fullinpartners.com/" + page + "?" + params
+    //window.location.replace("https://fullinpartners.com/" + page + "?" + params)
 }
